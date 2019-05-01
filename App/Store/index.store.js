b34fe8dcbd416036908b77a1da637b50b63ce185
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-05-01 22:24:05 +0530

    ffdfd

--- App/Store/index.store.js
+++ App/Store/index.store.js
@@ -1,17 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
-import { createLogger } from 'redux-logger'
 import saga from '../Saga/index.saga';
 import reducer from '../Reducers/index.reducer';
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
-const logger = createLogger({});
 
 // create a redux store with our reducer above and middleware
 const store = createStore(
     reducer,
-    compose(applyMiddleware(sagaMiddleware, logger))
+    compose(applyMiddleware(sagaMiddleware))
 );
 
 // run the saga
